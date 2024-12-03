6afd9607b538c0d246e4ad858fcc10e00d0c55d9
Tanguy Siri <tanguy.siri0901@gmail.com>
2024-12-03 16:46:49 +0100

    exo 3 animation

--- exo3.js
+++ exo3.js
@@ -1,2 +1,7 @@
 // Utiliser jQuery pour animer le <div> lorsqu'un bouton est cliqué.
 
+$("#animate").bind("click", function(){
+    $("#box").animate({width:"100%", opacity: 0.5,},1000);
+    $("#box").animate({height:"100vh",},1000);
+}
+)
