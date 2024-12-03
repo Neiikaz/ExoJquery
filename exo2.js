bfd0a211825b912cd95371f284f06ef0a7783eb6
Tanguy Siri <tanguy.siri0901@gmail.com>
2024-12-03 16:12:27 +0100

    Exo 2 rempli

--- exo2.js
+++ exo2.js
@@ -1,3 +1,5 @@
 /* Utiliser jQuery pour ajouter et retirer des classes sur le <div> 
 lorsque tu cliques sur un bouton.*/
-
+$('#toggleClass').bind('click', function(){
+    $("#content").toggleClass("red blue");
+})
