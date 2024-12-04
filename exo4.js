f77303e2bad11db2a0db4bbe93b4683f2866ac37
Tanguy Siri <tanguy.siri0901@gmail.com>
2024-12-04 13:56:17 +0100

    modif exo 4

--- exo4.js
+++ exo4.js
@@ -3,8 +3,8 @@ les valeurs des champs lorsque le formulaire est soumis,
 et affiche un message avec ces valeurs*/
 $("#myForm").bind("click", function(event){
     event.preventDefault();
-    let name =$("#name").val;
-    let age =$("#age").val;
-    $("#result").html("nom est" + "" + name + "et l'age est" + "" + age);
+    let name =$("#name").val();
+    let age =$("#age").val();
+    $("#result").html(" Le nom est " + name + " et l'age est " + age + " ans");
 }
 )
