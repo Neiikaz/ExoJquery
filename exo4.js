6d0abf626c1d848a54b133f8510445ae094ed0ac
Tanguy Siri <tanguy.siri0901@gmail.com>
2024-12-04 13:52:42 +0100

    ajout exo 4

--- exo4.js
+++ exo4.js
@@ -1,4 +1,10 @@
 /* Utiliser jQuery pour récupérer 
 les valeurs des champs lorsque le formulaire est soumis, 
 et affiche un message avec ces valeurs*/
-
+$("#myForm").bind("click", function(event){
+    event.preventDefault();
+    let name =$("#name").val;
+    let age =$("#age").val;
+    $("#result").html("nom est" + "" + name + "et l'age est" + "" + age);
+}
+)
