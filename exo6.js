6c263746ce166341e4713d3d15c3f0000ea015e8
Tanguy Siri <tanguy.siri0901@gmail.com>
2024-12-04 14:08:54 +0100

    ajoute exo 6

--- exo6.js
+++ exo6.js
@@ -1,4 +1,11 @@
 /* Utilise jQuery pour afficher ou masquer les <div> 
 lorsque des boutons spécifiques sont cliqués.*/
-
+$("#show").bind("click", function(){ 
+    $(".box").show()
+}
+)
+$("#hide").bind("click", function(){
+    $(".box").hide()
+}
+)
 //
