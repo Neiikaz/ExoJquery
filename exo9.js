56a85c228dc7a35e4e1d20f4041b6ae3a78bb888
Tanguy Siri <tanguy.siri0901@gmail.com>
2024-12-04 14:33:56 +0100

    Finition exo

--- exo9.js
+++ exo9.js
@@ -1,1 +1,20 @@
 /* Utilisez les événements .hover() pour agrandir le carré au survol et le réduire lorsque la souris quitte. */
+// $('#hoverBox').bind('mouseenter', function(){
+//    $(this).css('transform', 'scale(1.5)');
+// }).bind('mouseleave', function(){
+//     $(this).css('transform', 'scale(1)');
+// });
+
+$("#hoverBox").mouseover( () => 
+    {
+        $("#hoverBox").animate({
+            width: "200px", opacity: 0.5 , height: "120px"
+        })
+    })
+    
+    $("#hoverBox").mouseout( () =>
+    {
+        $("#hoverBox").animate({
+            width: "100px", opacity: 0.5 , height: "100px"
+        })
+    })
