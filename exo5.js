6b2d0ef8f08cfd3703cf06ae100855b3f3bc64fb
Tanguy Siri <tanguy.siri0901@gmail.com>
2024-12-04 13:59:58 +0100

    ajout exo 5

--- exo5.js
+++ exo5.js
@@ -1,2 +1,5 @@
 /*Utiliser jQuery pour charger du contenu à partir d'un fichier texte externe 
 lorsque le bouton est cliqué.*/
+$("loadContent").bind("click", function(){
+    $("#content").load("texte.txt");
+})
